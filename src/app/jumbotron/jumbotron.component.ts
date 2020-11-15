f0f3ff5af4035dafed92fe11f705411b9dd13065
Mateusz Hałada <mhalada@craftware.biz>
2020-11-15 12:46:11 +0100

    added frontpage image and changed icons in jumbotron

--- src/app/jumbotron/jumbotron.component.ts
+++ src/app/jumbotron/jumbotron.component.ts
@@ -1,16 +1,14 @@
 import {Component, Input, OnInit} from '@angular/core';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
-import { faGlobeEurope } from '@fortawesome/free-solid-svg-icons';
+import { faHashtag } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-jumbotron',
   templateUrl: './jumbotron.component.html',
   styleUrls: ['./jumbotron.component.scss']
 })
 export class JumbotronComponent implements OnInit {
-  instagramIcon = faInstagram;
-  mailIcon = faEnvelope;
-  websiteIcon = faGlobeEurope;
+  instagramIcon = faHashtag;
+  mailIcon = faHashtag;
+  websiteIcon = faHashtag;
 
   @Input() biography;
   @Input() followers;
