914159e1e662795f8466f39a14b9b6d6b16d3b62
Mateusz Hałada <mhalada@craftware.biz>
2020-12-11 11:31:42 +0100

    added behaviorsubject, optimized requests

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,16 +1,30 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Post} from '../post.interface';
 import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {ActivatedRoute, Router} from '@angular/router';
+
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.scss']
 })
-export class GalleryComponent{
+export class GalleryComponent {
   @Input() posts: Post[];
   @Output() openModal = new EventEmitter<Post>();
   @Input() cols: number;
   @Input() gutterSize: string;
+
+  // constructor(private router: Router,
+  //             private route: ActivatedRoute) {
+  //   this.route.paramMap.subscribe(paramMap => {
+  //     if (!paramMap.has('placeId')) {
+  //       this.router.navigateByUrl('');
+  //       return;
+  //     }
+  //     const placeId = paramMap.get('placeId');
+  //     this.showModal(placeId);
+  //   };
+  // }
 
   showModal(post: Post): void {
     this.openModal.emit(post);
--- src/app/photos-service/photos.service.ts
+++ src/app/photos-service/photos.service.ts
@@ -1,17 +1,66 @@
 import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
+import {BehaviorSubject, Observable} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
+import {Post} from '../post.interface';
+import {map, tap} from 'rxjs/operators';
+
+interface ProfileData {
+  posts: Post[];
+  biography: string;
+  followers: number;
+  postsCount: number;
+  profilePicture: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhotosService {
+  // instagram is lazy-loading photos so with api call you can get only 12 of them
+  private postsLoaded = 12;
+  private profileInfo = new BehaviorSubject<ProfileData>(null);
 
   constructor(public http: HttpClient) {
   }
 
-  getProfileData(): Observable<any> {
-    return this.http.get(environment.url);
+  get profileData(): Observable<ProfileData> {
+    return this.profileInfo.asObservable();
+  }
+
+  // tslint:disable-next-line:typedef
+  fetchProfileData() {
+    return this.http.get(environment.url)
+      .pipe(
+        map((res: any) => res.graphql.user),
+        tap(data => {
+          const posts = [];
+
+          const images = data.edge_owner_to_timeline_media.edges.map(post =>
+            post.node.display_url
+          );
+          const texts = data.edge_owner_to_timeline_media.edges.map(post =>
+            post.node.edge_media_to_caption.edges.map(d => d.node.text)
+          );
+          const ids = data.edge_owner_to_timeline_media.edges.map(post =>
+            post.node.id);
+          for (let i = 0; i < this.postsLoaded; i++) {
+            posts.push({
+              image: images[i],
+              text: texts[i],
+              id: ids[i]
+            });
+          }
+
+          const profileData: ProfileData = {
+            posts,
+            biography: data.biography.split('e-mail', 1),
+            followers: data.edge_followed_by.count,
+            postsCount: data.edge_owner_to_timeline_media.count,
+            profilePicture: data.profile_pic_url_hd
+          };
+          this.profileInfo.next(profileData);
+        })
+      );
   }
 }
